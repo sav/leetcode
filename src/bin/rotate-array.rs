b5f705954ed8c5a5d6f25d9539edd76803950b54
Savio Sena <savio.sena@gmail.com>
2024-05-31 05:25:28 -0300

    Add `rotate-array`.
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ src/bin/rotate-array.rs
@@ -0,0 +1,39 @@
+// rotate-array.rs, My solutions to various Leetcode puzzles.
+// Copyright (C) 2024, Savio Sena <savio.sena@gmail.com>
+
+//! # 189. Rotate Array
+//!
+//!  - https://leetcode.com/problems/rotate-array
+
+use leetcode::*;
+use std::error::Error;
+
+struct Solution;
+
+impl Solution {
+    pub fn rotate(v: &mut Vec<i32>, k: i32) {
+        let n = v.len();
+        let k = k as usize % n;
+        let w: Vec<i32> = v[n - k..n].to_vec();
+
+        for i in (0..n - k).rev() {
+            v[k + i] = v[i];
+        }
+
+        for i in 0..k {
+            v[i] = w[i];
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = std::io::stdin().lock();
+
+    let Vector(mut nums) = input.read_line_as::<Vector<i32>>()?;
+    let k: i32 = input.read_line_as()?;
+
+    Solution::rotate(&mut nums, k);
+    println!("{:?}", nums);
+
+    Ok(())
+}
