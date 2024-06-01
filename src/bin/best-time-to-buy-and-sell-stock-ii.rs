35e96a101de8a29ea2e02f2aca6f7cbd603553a8
Savio Sena <savio.sena@gmail.com>
2024-06-01 09:11:15 -0300

    Add `best-time-to-buy-and-sell-stock-ii`.
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ src/bin/best-time-to-buy-and-sell-stock-ii.rs
@@ -0,0 +1,37 @@
+// best-time-to-buy-and-sell-stock-ii.rs, My solutions to various Leetcode puzzles.
+// Copyright (C) 2024, Savio Sena <savio.sena@gmail.com>
+
+//! # 122. Best Time to Buy and Sell Stock II
+//!
+//!  - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii
+
+use leetcode::*;
+use std::error::Error;
+
+struct Solution;
+
+impl Solution {
+    pub fn max_profit(v: Vec<i32>) -> i32 {
+        let n = v.len();
+        let mut profit = 0;
+
+        for i in 0..n-1 {
+            if v[i+1] - v[i] > 0 {
+                profit += v[i+1] - v[i]; 
+            }
+        }
+
+        profit
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = std::io::stdin().lock();
+    let Vector(prices) = input.read_line_as::<Vector<i32>>()?;
+
+    println!("{prices:?}");
+    let day = Solution::max_profit(prices);
+    println!("{day}");
+
+    Ok(())
+}
