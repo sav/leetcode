0c2b04fc633b412588cb86eca20a4ba006986567
Savio Sena <savio.sena@gmail.com>
2024-06-06 11:51:13 -0300

    Add `length-of-last-word`.
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ src/bin/length-of-last-word.rs
@@ -0,0 +1,28 @@
+// length-of-last-word.rs, My solutions to various Leetcode puzzles.
+// Copyright (C) 2024, Savio Sena <savio.sena@gmail.com>
+
+//! # 58. Length of Last Word
+//!
+//!  - https://leetcode.com/problems/length-of-last-word
+
+use leetcode::*;
+use std::error::Error;
+
+struct Solution;
+
+impl Solution {
+    pub fn length_of_last_word(s: String) -> i32 {
+        s.trim().split(' ').rev().next().unwrap().len() as i32
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = std::io::stdin().lock();
+
+    let s = input.read_line_as::<String>()?;
+
+    let n = Solution::length_of_last_word(s);
+    println!("{}", n);
+
+    Ok(())
+}
