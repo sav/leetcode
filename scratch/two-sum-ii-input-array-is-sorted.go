642d5ac7ad251459b2b711ea4d428cc66d0e754a
Savio Sena <savio.sena@gmail.com>
2024-06-07 16:31:48 -0300

    Add `two-sum-ii-input-array-is-sorted.go`.
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ scratch/two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,23 @@
+package main
+
+func twoSum(v []int, sum int) []int {
+	n := len(v)
+	i, j := 0, n-1
+
+	for i < j {
+		if v[i]+v[j] == sum {
+			break
+		} else if v[i]+v[j] > sum {
+			j--
+		} else {
+			i++
+		}
+	}
+
+	return []int{i + 1, j + 1}
+}
+
+func main() {
+	Assert(twoSum, []int{2, 7, 11, 15}, 9, []int{1, 2})
+	Assert(twoSum, []int{1, 2, 3, 5, 9}, 8, []int{3, 4})
+}
