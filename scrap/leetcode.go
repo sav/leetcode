82930526321d3cd5f34c7152df531cc14045d159
Savio Sena <savio.sena@gmail.com>
2024-06-07 13:11:58 -0300

    Add `scrap/leetcode.go`.
    A helper library to scrap code in Go.
    
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ scrap/leetcode.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type Example []interface{}
+
+func Assert(fn interface{}, args ...interface{}) {
+	expected := args[len(args)-1]
+
+	args = args[:len(args)-1]
+
+	fnValue := reflect.ValueOf(fn)
+	argsValues := make([]reflect.Value, len(args))
+
+	for i, arg := range args {
+		argsValues[i] = reflect.ValueOf(arg)
+	}
+
+	result := fnValue.Call(argsValues)
+
+	if len(result) != 1 {
+		panic("Function must return exactly one value")
+	}
+
+	actual := result[0].Interface()
+
+	if !reflect.DeepEqual(actual, expected) {
+		panic(fmt.Sprintf("test failed: %v: expected=%v, got=%v",
+			args, expected, actual))
+	}
+
+	fmt.Printf("test passed: %v -> %v\n", args, actual)
+}
