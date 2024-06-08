69be44cb10895a29906cc79c339ed50851710583
Savio Sena <savio.sena@gmail.com>
2024-06-08 10:27:11 -0300

    Add `word-pattern.go`.
    Signed-off-by: Savio Sena <savio.sena@gmail.com>

---
+++ scratch/word-pattern.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"strings"
+)
+
+func wordPattern(pattern string, s string) bool {
+	words := strings.Split(s, " ")
+
+	patternRunes := []rune(pattern)
+
+	if len(words) != len(patternRunes) {
+		return false
+	}
+
+	patternToWord := make(map[rune]string)
+	wordToPattern := make(map[string]rune)
+
+	for i, word := range words {
+		currWord, wordExists := patternToWord[patternRunes[i]]
+		if wordExists && word != currWord {
+			return false
+		} else if !wordExists {
+			patternToWord[patternRunes[i]] = word
+		}
+
+		currPattern, patternExists := wordToPattern[word]
+		if patternExists && patternRunes[i] != currPattern {
+			return false
+		} else if !patternExists {
+			wordToPattern[word] = patternRunes[i]
+		}
+	}
+
+	return true
+}
+
+func main() {
+	Assert(wordPattern, "abba", "cat dog dog cat", true)
+	Assert(wordPattern, "abbr", "cat dog dog cat", false)
+}
